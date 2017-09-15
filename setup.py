cfc7ec2f29acd1dd158575a5b52a6dce0279c256
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-09-15 14:48:01 +0200

    Add setup.py with package definitions

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+
+def get_packages(package):
+    return [
+        dirpath for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, '__init__.py'))
+    ]
+
+
+setup(
+    name='django-templated-mail',
+    version='0.1.0',
+    packages=get_packages('templated_mail'),
+    license='MIT',
+    author='Sunscrapers',
+    description='Send emails using Django template system.',
+    author_email='info@sunscrapers.com',
+    long_description=readme,
+    install_requires=[],
+    include_package_data=True,
+    url='https://github.com/sunscrapers/django-templated-mail',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
